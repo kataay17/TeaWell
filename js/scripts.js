a522137bf58bbe140f83c09fd75747495404c118
Daniel <nissimw23@gmail.com>
2015-10-29 19:10:54 -0400

    update 01293

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+            $(".index").mouseenter(function() {
+                $("#overlay").show();
+            });
+            $(".index").mouseleave(function() {
+                $("#overlay").hide();
+            });
+        });
+        $(document).ready(function() {
+            $(".quiz").mouseenter(function() {
+                $("#overly").show();
+            });
+            $(".quiz").mouseleave(function() {
+                $("#overly").hide();
+            });
+        });
